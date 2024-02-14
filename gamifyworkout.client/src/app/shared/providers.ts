76cc1b1862665c763cdaaf6ac271ef9de847d4d8
Jebb Burditt <Jebb.Burditt@quartech.com>
2024-02-13 17:35:06 -0800

    add error handler and http interceptor

--- gamifyworkout.client/src/app/shared/providers.ts
+++ gamifyworkout.client/src/app/shared/providers.ts
@@ -1,6 +1,6 @@
 // dependency injection wireup for services that can be changed or customized for different environments
 
-import { Provider } from "@angular/core";
+import { ErrorHandler, Provider } from "@angular/core";
 import { ConfigService } from "@app/core/services/config/config-service.interface";
 import { JsonConfigService } from "@app/core/services/config/json-config.service";
 import { LoggingService } from "@app/core/services/logging/logging-service.interface";
@@ -10,6 +10,21 @@ import { provideLumberjackConsoleDriver } from "@ngworker/lumberjack/console-dri
 import { LoggingFactory } from "@app/core/services/logging/logging.factory";
 import { HotToastService, provideHotToastConfig } from "@ngneat/hot-toast";
 import { ToastService } from "@app/core/services/toast/toast-service.interface";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { ErrorHandlerService } from "@app/core/services/error-handler-service";
+import { HttpConfigInterceptor } from "@app/core/interceptors/httpconfig.interceptor";
+
+export function provideHttpInterceptor(): Provider {
+  return {
+    provide: HTTP_INTERCEPTORS,
+    useClass: HttpConfigInterceptor,
+    multi: true
+  }
+}
+
+export function provideErrorHandler(): Provider {
+  return { provide: ErrorHandler, useClass: ErrorHandlerService };
+}
 
 export function provideConfigService(): Provider {
   return {
--- gamifyworkout.client/src/main.ts
+++ gamifyworkout.client/src/main.ts
@@ -7,7 +7,7 @@ import { routes } from '@app/app.routes';
 import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 import { ConfigService } from '@app/core/services/config/config-service.interface';
-import { provideConfigService, provideLoggingService, provideToastService } from '@app/shared/providers';
+import { provideConfigService, provideErrorHandler, provideHttpInterceptor, provideLoggingService, provideToastService } from '@app/shared/providers';
 
 export function initializeApp(configService: ConfigService/*, http: HttpClient, authService: AuthenticationService*/) {
   return (): Observable<void> => {
@@ -27,6 +27,8 @@ bootstrapApplication(AppComponent, {
     },
     provideAnimations(),
     provideRouter(routes),
+    provideErrorHandler(),
+    provideHttpInterceptor(),
     provideHttpClient(withInterceptorsFromDi()),
     provideConfigService(),
     provideLoggingService(),

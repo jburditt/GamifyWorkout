9de2708d90b4cd04ee6f40a213942cf6f3366cab
Jebb Burditt <jebb.burditt@gmail.com>
2025-06-09 13:00:18 -0700

    start of player observer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -9,6 +9,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MenuComponent } from './shared/components/menu/menu.component';
+import { Store } from '@ngrx/store';
+import { Player } from '@features/rpg/model/player';
 
 @Component({
   selector: 'app-root',
@@ -23,19 +25,24 @@ import { MenuComponent } from './shared/components/menu/menu.component';
 })
 export class AppComponent implements OnInit {
 
-  public projectName: string = 'Gamify Workout';
-
   private readonly _loggingService: LoggingService;
 
   menuItemList: Array<MenuItem> = [
     new MenuItem('Weather', '/user/weather', 'assignment'),
     new MenuItem('Admin', '/admin', 'settings'),
     new MenuItem('User', '/user', 'contact_mail'),
+    new MenuItem('Dashboard', '/user/dashboard', 'dashboard'),
   ];
 
-  constructor(private loggingFactory: LoggingFactory)
+  public player!: Player;
+
+  constructor(private store: Store<{ player: Player }>, private loggingFactory: LoggingFactory)
   {
     this._loggingService = this.loggingFactory.create(this.constructor.name);
+    store.select('player').subscribe((player) => {
+      console.log('player', player);
+      this.player = player;
+    });
   }
 
   ngOnInit() {

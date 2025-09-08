60bd9de58b1206f0ca2be1027f174a63bb7919b4
Jebb Burditt <jebb.burditt@gmail.com>
2025-09-08 15:43:10 -0700

    load user on edit page (does not edit yet)

--- frontend/src/app/modules/user/pages/form.component.ts
+++ frontend/src/app/modules/user/pages/form.component.ts
@@ -6,6 +6,7 @@ import { EntityService, UserService } from '@app/api/services';
 import { DatePickerComponent } from '@app/shared/components/datepicker/date-picker.component';
 import { Model } from "survey-core";
 import { SurveyModule } from 'survey-angular-ui';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     templateUrl: 'form.component.html',
@@ -22,34 +23,42 @@ export class FormPageComponent implements OnInit {
   });
   surveyModel!: Model;
 
-  constructor(private entityService: EntityService, private userService: UserService) { }
+  constructor(private entityService: EntityService, private userService: UserService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
     this.entityService.apiEntityGetentityGet({ entityName: "User" }).subscribe((entities) => {
       console.log(entities);
     });
 
     const surveyJson = {
       elements: [{
-        name: "FirstName",
+        name: "firstName",
         title: "First Name:",
         type: "text"
       }, {
-        name: "LastName",
+        name: "lastName",
         title: "Last Name:",
         type: "text"
       }, {
-        name: "Username",
+        name: "username",
         title: "Username:",
         type: "text"
       }, {
-        name: "Email",
+        name: "email",
         title: "Email:",
         type: "text"
       }]
     };
 
-    const survey = new Model(surveyJson);
+    let survey = new Model(surveyJson);
+if (id) {
+  this.userService.apiUserIdGet({ id: id }).subscribe((user) => {
+    console.log(user);
+    survey.data = user;
+});
+}
     survey.onComplete.add((sender, options) => {
       console.log("Survey results: " + survey.data);
       this.userService.apiUserPost({ body: survey.data }).subscribe((user) => {

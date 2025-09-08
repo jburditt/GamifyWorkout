051b716afe68215a01385d24fcbe27c323c03c2d
Jebb Burditt <jebb.burditt@gmail.com>
2025-09-08 16:27:34 -0700

    add unit test project

---
+++ Tests/Test1.cs
@@ -0,0 +1,20 @@
+﻿namespace Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void Test_Add_Multiple_Values()
+    {
+        //var dict = new MultiValueDictionary<string, string>();
+        //dict.Add("animal", "tiger");
+        //dict.Add("animal", "lion");
+
+        //Assert.AreEqual(2, dict.Flatten().Count());
+        //Assert.AreEqual(2, dict.Get("animal").Count());
+        //Assert.IsTrue(dict.Get("animal").Contains("tiger"));
+        //Assert.IsTrue(dict.Get("animal").Contains("lion"));
+        //Assert.IsTrue(dict.Flatten().Any(kv => kv.Key == "animal" && kv.Value == "tiger"));
+        //Assert.IsTrue(dict.Flatten().Any(kv => kv.Key == "animal" && kv.Value == "lion"));
+    }
+}

cf4f3df9b04443df144cf03011671b8c7177d820
Jebb Burditt <jebb.burditt@gmail.com>
2025-09-08 17:50:47 -0700

    improve styles of search users

--- frontend/src/app/modules/user/pages/form.component.ts
+++ frontend/src/app/modules/user/pages/form.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatNativeDateModule } from '@angular/material/core';
 import { EntityService, UserService } from '@app/api/services';
 import { Model } from "survey-core";
 import { SurveyModule } from 'survey-angular-ui';
 import { ActivatedRoute } from '@angular/router';
+import { PlainLight } from 'survey-core/themes';
 
 @Component({
     templateUrl: 'form.component.html',
-    imports: [FormsModule, ReactiveFormsModule, SurveyModule, MatNativeDateModule]
+    imports: [SurveyModule]
 })
 export class FormPageComponent implements OnInit {
   surveyModel!: Model;
@@ -44,13 +43,16 @@ export class FormPageComponent implements OnInit {
     };
 
     let survey = new Model(surveyJson);
-if (id) {
-  this.userService.apiUserIdGet({ id: id }).subscribe((user) => {
-    console.log(user);
-    survey.data = user;
-});
-}
-// TODO handle edit instead of always adding new user
+    survey.applyTheme(PlainLight);
+    survey.completeText = "Add User";
+    survey.completedHtml = "User added successfully!";
+    if (id) {
+      this.userService.apiUserIdGet({ id: id }).subscribe((user) => {
+        console.log(user);
+        survey.data = user;
+      });
+    }
+    // TODO handle edit instead of always adding new user
     survey.onComplete.add((sender, options) => {
       console.log("Survey results: " + survey.data);
       this.userService.apiUserPost({ body: survey.data }).subscribe((user) => {

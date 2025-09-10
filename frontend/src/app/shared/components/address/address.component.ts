1868d437580b2dea1eecdb1d1cf47841c1f38cff
Jebb Burditt <jebb.burditt@gmail.com>
2025-09-09 17:19:03 -0700

    code cleanup

--- frontend/src/app/shared/components/address/address.component.ts
+++ frontend/src/app/shared/components/address/address.component.ts
@@ -11,11 +11,4 @@ import { MatInputModule } from '@angular/material/input';
 })
 export class AddressComponent  {
   @Input() addressForm!: FormGroup;
-  // addressForm = new FormGroup({
-  //   addressLine1: new FormControl('', [Validators.required]),
-  //   addressLine2: new FormControl(''),
-  //   city: new FormControl('', [Validators.required]),
-  //   province: new FormControl('', [Validators.required]),
-  //   postalCode: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{5}(?:-[0-9]{4})?$')])
-  // });
 }

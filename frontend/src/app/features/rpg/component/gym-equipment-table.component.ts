56a79877ce9fad509017b934ce3f4c42220bece1
Jebb Burditt <jebb.burditt@gmail.com>
2025-09-21 13:15:36 -0700

    start of multi select checkboxes

--- frontend/src/app/features/rpg/component/gym-equipment-table.component.ts
+++ frontend/src/app/features/rpg/component/gym-equipment-table.component.ts
@@ -7,27 +7,46 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { Equipment } from '@app/api/models';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     templateUrl: 'gym-equipment-table.component.html',
-    imports: [MatIconModule, MatButtonModule, MatPaginatorModule, MatTableModule],
+    imports: [MatIconModule, MatButtonModule, MatPaginatorModule, MatTableModule, MatCheckboxModule],
     selector: 'gym-equipment-table'
 })
 export class GymEquipmentTableComponent {
   private readonly _loggingService: LoggingService;
+
   @Input() canAddRow: boolean = false;
+  @Input() canSelect: boolean = false;
+
   @Input() dataSource = new MatTableDataSource<Equipment>();
   displayedColumns: string[] = ['icon', 'equipment', 'edit'];
 
   addGymEquipment = output<boolean>();
 
-  constructor(private loggingFactory: LoggingFactory)
+  formGroup!: FormGroup;
+
+  constructor(private loggingFactory: LoggingFactory, private formBuilder: FormBuilder)
   {
     this._loggingService = this.loggingFactory.create(this.constructor.name);
   }
 
   ngOnInit() {
     this._loggingService.debug('GymEquipmentTableComponent initialized');
+    this.formGroup = this.formBuilder.group({ equipment: this.formBuilder.array([]) });
+  }
+
+  onChange(event: any) {
+    const equipment = <FormArray>this.formGroup.get('equipment') as FormArray;
+
+    if(event.checked) {
+      equipment.push(new FormControl(event.source.value))
+    } else {
+      const i = equipment.controls.findIndex(x => x.value === event.source.value);
+      equipment.removeAt(i);
+    }
   }
 
   openDialog() {

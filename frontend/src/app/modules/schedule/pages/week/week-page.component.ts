b25f5a95452df0ef28a96815ef9c7bc79778c07f
Jebb Burditt <jebb.burditt@gmail.com>
2025-09-22 10:27:48 -0700

    drag and drop cleanup

--- frontend/src/app/modules/schedule/pages/week/week-page.component.ts
+++ frontend/src/app/modules/schedule/pages/week/week-page.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
 import { WeekContainerComponent } from "@app/features/rpg/component/week-container/week-container.component";
 import { CdkDragDrop, moveItemInArray, transferArrayItem, copyArrayItem, CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
 import { CdkDropListGroup } from "@angular/cdk/drag-drop";
 
 @Component({
-  imports: [WeekContainerComponent, CdkDropList, CdkDrag, CdkDropListGroup],
+  imports: [WeekContainerComponent, CdkDropList, CdkDrag, CdkDropListGroup, forwardRef(() => WeekdayDropContainer)],
   templateUrl: './week-page.component.html',
   styleUrl: './week-page.component.css'
 })
@@ -12,6 +12,28 @@ export class WeekPageComponent {
   activity = ['Rest', 'Any', 'Cardio', 'Core', 'Chest', 'Back', 'Shoulders', 'Arms', 'Legs'];
   monday: Array<string> = [];
   tuesday: Array<string> = [];
+  wednesday: Array<string> = [];
+  thursday: Array<string> = [];
+  friday: Array<string> = [];
+  saturday: Array<string> = [];
+  sunday: Array<string> = [];
+}
+
+@Component({
+  selector: 'app-weekday-drop-container',
+  template: `
+    <div class="weekdayColumn" cdkDropList [id]="id" [cdkDropListData]="data" (cdkDropListDropped)="drop($event)">
+      @for (item of data; track item) {
+        <div class="activity activity-{{ item.toLowerCase() }}" cdkDrag>{{ item }}</div>
+      }
+    </div>
+  `,
+  styleUrls: ['week-page.component.css'],
+  imports: [CdkDropList, CdkDrag],
+})
+export class WeekdayDropContainer {
+  @Input() data: Array<string> = [];
+  @Input() id!: string;
 
   drop(event: CdkDragDrop<string[]>) {
     console.log("event", event);

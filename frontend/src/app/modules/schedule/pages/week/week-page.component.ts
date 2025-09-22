cbb84f7913eb4f47b2be3baa0bbb6f28a0025379
Jebb Burditt <jebb.burditt@gmail.com>
2025-09-22 11:03:13 -0700

    add buttons to weekly schedule

--- frontend/src/app/modules/schedule/pages/week/week-page.component.ts
+++ frontend/src/app/modules/schedule/pages/week/week-page.component.ts
@@ -2,9 +2,10 @@ import { Component, Input, forwardRef } from '@angular/core';
 import { WeekContainerComponent } from "@app/features/rpg/component/week-container/week-container.component";
 import { CdkDragDrop, moveItemInArray, transferArrayItem, copyArrayItem, CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
 import { CdkDropListGroup } from "@angular/cdk/drag-drop";
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  imports: [WeekContainerComponent, CdkDropList, CdkDrag, CdkDropListGroup, forwardRef(() => WeekdayDropContainer)],
+  imports: [WeekContainerComponent, MatButtonModule, CdkDropList, CdkDrag, CdkDropListGroup, forwardRef(() => WeekdayDropContainer)],
   templateUrl: './week-page.component.html',
   styleUrl: './week-page.component.css'
 })

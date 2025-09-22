d57952a7f1a9accce2d4377c848224b2cd1b66f3
Jebb Burditt <jebb.burditt@gmail.com>
2025-09-22 12:39:21 -0700

    dialog callback cleanup

--- frontend/src/app/modules/schedule/dialogs/add-weekly-schedule.ts
+++ frontend/src/app/modules/schedule/dialogs/add-weekly-schedule.ts
@@ -1,7 +1,6 @@
-import { ChangeDetectionStrategy, Component, Inject, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { EquipmentService, GymEquipmentService } from '@app/api/services';
+import { MatDialogModule } from '@angular/material/dialog';
 import { TextboxComponent } from '@app/shared/components/form/textbox/textbox.component';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
@@ -9,25 +8,14 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   templateUrl: 'add-weekly-schedule.html',
-  imports: [MatButtonModule, MatDialogModule, MatInputModule, TextboxComponent, MatSlideToggleModule],
-  //changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MatButtonModule, MatDialogModule, MatInputModule, TextboxComponent, MatSlideToggleModule]
 })
-export class AddWeeklyScheduleDialog implements OnInit {
+export class AddWeeklyScheduleDialog {
   form: FormGroup = new FormGroup({
     name: new FormControl('', {
       validators: [Validators.required, Validators.minLength(3), Validators.maxLength(50)]
     })
   });
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { gymId: string, equipmentIds: string }, private equipmentService: EquipmentService, private gymEquipmentService: GymEquipmentService) { }
-
-  ngOnInit(): void {
-    this.equipmentService.apiEquipmentGet().subscribe((equipment) => {
-
-    });
-  }
-
-  add(): void {
-
-  }
+  constructor() { }
 }

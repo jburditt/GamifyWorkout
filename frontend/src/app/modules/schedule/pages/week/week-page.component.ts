439ecdae2eb093be1ca7c445428d1cd9871cfd41
Jebb Burditt <jebb.burditt@gmail.com>
2025-09-22 08:57:59 -0700

    style weekly schedule

--- frontend/src/app/modules/schedule/pages/week/week-page.component.ts
+++ frontend/src/app/modules/schedule/pages/week/week-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { WeekContainerComponent } from "@app/features/rpg/component/week-container/week-container.component";
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
 
 @Component({
-  imports: [WeekContainerComponent],
+  imports: [WeekContainerComponent, CdkDropList, CdkDrag],
   templateUrl: './week-page.component.html',
   styleUrl: './week-page.component.css'
 })
 export class WeekPageComponent {
-
+  activity = ['Rest', 'Any', 'Cardio', 'Core', 'Chest', 'Back', 'Shoulders', 'Arms', 'Legs']
+  
 }

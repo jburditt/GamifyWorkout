5c4a952f1118eb8187d489a8dbb9ad68cc5f3e5e
Jebb Burditt <jebb.burditt@gmail.com>
2026-02-05 16:51:47 -0800

    try adding database context DI

--- api/Functions/Program.cs
+++ api/Functions/Program.cs
@@ -1,5 +1,8 @@
+using Core;
+using Database;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,5 +13,14 @@ builder.ConfigureFunctionsWebApplication();
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+
+builder.Services.AddDbContextFactory<EfDbContext>(options => options.UseSqlServer("Server=tcp:sql-gamifyworkout.database.windows.net,1433;Initial Catalog=sqldb-gamifyworkout;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication='Active Directory Default';"));
+builder.Services.AddTransient<Repository, EFRepository>();
+//using (var scope = app.Services.CreateScope())
+//{
+//    var services = scope.ServiceProvider;
+//    var context = services.GetRequiredService<EfDbContext>();
+//    context.Database.Migrate();
+//}
 
 builder.Build().Run();
